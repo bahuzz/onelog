2bc85bc8c78097da2c67f110fa34bf531db1dcc7
bahuzz <dbahuzz@gmail.com>
2017-02-28 18:06:58 +0300

    treplate 2 cols

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-
+	$('.sidemenu-btn').on('click',function(){
+		$('.sidemenu').toggleClass('sidemenu-closed');
+		$('.template-wrapper').toggleClass('template-wrapper-menu-open');
+		$(this).toggleClass('sidemenu-btn-close');
+	});
+	$(".sidemenu-list").mCustomScrollbar();
 });
